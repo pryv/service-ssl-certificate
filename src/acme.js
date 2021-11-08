c1880ee9729938978592138ab4912af251ada7b7
Ilia Kebets <kebetsi@gmail.com>
2021-11-08 12:54:50 +0100

    add wait time for DNS requests

--- src/acme.js
+++ src/acme.js
@@ -1,4 +1,5 @@
 const dns = require('dns-dig');
+const { getLogger, getConfig } = require('@pryv/boiler');
 const {
   updateDnsTxtRecord,
   rebootServices,
@@ -8,6 +9,9 @@ const {
  * This function is declared separately, because it is referenced in the tests
  */
 module.exports.challengeCreateFn = async function (domain, token, settings, authz, challenge, keyAuthorization) {
+  const logger = getLogger('acme');
+  const config = await getConfig();
+  const dnsWaitTime = config.get('acme:dnsWaitTime');
   await updateDnsTxtRecord(token, keyAuthorization, settings);
   await rebootServices(token, ['pryvio_dns']);
 
@@ -15,7 +19,14 @@ module.exports.challengeCreateFn = async function (domain, token, settings, auth
 
   let isTxtRecordSet = false;
   while (! isTxtRecordSet) {
+    logger.info(`Checking DNS challenge ${txtRecordHostname}`);
     const txtRecords = await dns.resolveTxt(txtRecordHostname);
+    logger.info(`Obtained ${txtRecords}`);
     if (txtRecords.length > 0 && txtRecords[0] === keyAuthorization) isTxtRecordSet = true;
+    await sleep(dnsWaitTime);
   }
 };
+
+function sleep(milliseconds) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
