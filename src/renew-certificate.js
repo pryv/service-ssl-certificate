df833ad647d39b10918eb2c45d75d7c2e4aa8122
Perki <perki@pryv.com>
2023-05-01 12:03:54 +0200

    Adding email checks and release 1.1.4

--- src/renew-certificate.js
+++ src/renew-certificate.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (C) 2012–2022 Pryv S.A. https://pryv.com - All Rights Reserved
+ * Copyright (C) 2012–2023 Pryv S.A. https://pryv.com - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  */
