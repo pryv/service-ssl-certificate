e044df1029c36aec040cbbd73c543aad0439cfa6
Perki <perki@pryv.com>
2025-02-16 15:25:02 +0100

    Open sourcing

--- test/acceptance/renewal.test.js
+++ test/acceptance/renewal.test.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (C) Pryv S.A. https://pryv.com
+ * Copyright (C) Pryv https://pryv.com
  * This file is part of Pryv.io and released under BSD-Clause-3 License
  * Refer to LICENSE file
  */
